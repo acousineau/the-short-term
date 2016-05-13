d468dd1f80aa70293e510ae852af3961d94b7703
acousineau <andrew.cousineau@gmail.com>
2016-05-13 09:37:09 -0400

    Remove timer

--- js/index.js
+++ js/index.js
@@ -1,14 +1,1 @@
-var countdown = function() {
-  var now = moment();
-  var releaseDate = moment('2016-05-13');
-  var duration = moment.duration(releaseDate - now, 'milliseconds');
 
-  document.getElementById("countdown").innerHTML = 
-    duration.days() + ':' +
-    duration.hours() + ':' +
-    duration.minutes() + ':' +
-    duration.seconds()
-}
-
-countdown();
-setInterval(countdown, 1000);
